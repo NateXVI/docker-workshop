4ebb738616177c1028c93d037f56e8f083b36117
Nate Diven <natediven1@gmail.com>
2025-04-25 18:59:14 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import "./index.css";
+import { APITester } from "./APITester";
+
+import logo from "./docker.svg";
+import reactLogo from "./react.svg";
+
+export function App() {
+  return (
+    <div className="max-w-7xl mx-auto p-8 text-center relative z-10">
+      <div className="flex justify-center items-center gap-8 mb-8">
+        <img
+          src={logo}
+          alt="Bun Logo"
+          className="h-24 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa] scale-120"
+        />
+      </div>
+
+      <h1 className="text-5xl font-bold my-4 leading-tight">Bun + Docker</h1>
+      <p>Bun app hosted in a Docker container.</p>
+      <APITester />
+    </div>
+  );
+}
+
+export default App;
